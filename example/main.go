8d9c5b647c827eec9431e615d86c22356fad57d7
keigodasu <keigodasu05240524@gmail.com>
2021-03-28 17:39:50 +0900

    add sample output to example

--- example/main.go
+++ example/main.go
@@ -15,18 +15,43 @@ func main() {
 		"primary_flag":  hay.NewBool(true),
 		"secondly_flag": hay.NewBool(false),
 	}
-
 	entity.Tags = tags
-
-	jsonEncoded, _ := entity.MarshallJSON()
-
-	fmt.Println(string(jsonEncoded))
 
 	grid := hay.Grid{
 		Meta:     map[string]string{"ver": "3.0", "projName": "test"},
 		Entities: []*hay.Entity{entity},
 	}
-	jsonEncoded, _ = grid.MarshallJSON()
+	jsonEncoded, _ := grid.MarshallJSON()
 
 	fmt.Println(string(jsonEncoded))
+	/*
+	{
+	  "meta": {
+	    "projName": "test",
+	    "ver": "3.0"
+	  },
+	  "cols": [
+	    {
+	      "name": "secondly_flag"
+	    },
+	    {
+	      "name": "unit01_name"
+	    },
+	    {
+	      "name": "unit02_name"
+	    },
+	    {
+	      "name": "primary_flag"
+	    }
+	  ],
+	  "rows": [
+	    {
+	      "primary_flag": "b:true",
+	      "secondly_flag": "b:false",
+	      "unit01_name": "s:xxxxx",
+	      "unit02_name": "s:yyyyy"
+	    }
+	  ]
+	}
+	 */
 }
